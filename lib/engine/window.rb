9fd78da8f8fa1936a4b6b76986a3abad4f5281bd
Alastair Gilles <agilles91@gmail.com>
2019-10-25 12:52:26 +0200

    changed some methods names to snake_case

--- lib/engine/window.rb
+++ lib/engine/window.rb
@@ -16,16 +16,16 @@ class Window
   end
 
   def init &key_callback
-    setErrorCallback
+    set_error_callback
 
     @glfw_init = glfwInit() == GLFW_TRUE
 
     @handle = glfwCreateWindow(@width, @height, @title, nil, nil)
 
     if block_given?
-      setKeyCallback &key_callback
+      set_key_callback &key_callback
     else
-      setKeyCallback
+      set_key_callback
     end
   end
 
@@ -41,14 +41,14 @@ class Window
   end
 
   private
-  def setErrorCallback
+  def set_error_callback
     @error_callback = GLFW::create_callback(:GLFWerrorfun) do |int, message|
       STDERR.puts "GLFW ERROR: #{int} -- #{message}"
     end
     glfwSetErrorCallback(@error_callback)
   end
 
-  def setKeyCallback
+  def set_key_callback
     @key_callback = GLFW::create_callback(:GLFWkeyfun) do |wind, key, sccd, act, mod|
       if key == GLFW_KEY_ESCAPE && act = GLFW_PRESS
         glfwSetWindowShouldClose(wind, GLFW_TRUE)
