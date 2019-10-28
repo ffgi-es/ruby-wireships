e69f24107407a396ba9a0ae6b58fcbefc705520f
Alastair Gilles <agilles91@gmail.com>
2019-10-28 10:41:17 +0100

    Added syncronised game_loop to GameEngine
    GameEngine now has an attribute target_fps which denotes how often it
    will attempt to update the game state by call update on the game_logic
    attribute.
    
    game_logic#update must explicitly return either true (continue looping) or false
    (exit the loop) otherwise and error will be raised. nil or any other
    value will not be accepted.

--- lib/engine/gameengine.rb
+++ lib/engine/gameengine.rb
@@ -1,21 +1,50 @@
 require 'window'
+require 'timer'
+
+class UnknownRunningStatusError < StandardError
+end
 
 class GameEngine
-  attr_reader :window, :game_logic
+  attr_reader :window, :game_logic, :timer
+  attr_accessor :target_fps
   def initialize window_title, width, height, v_sync, logic
     @window = Window.new window_title, width, height, v_sync
     @game_logic = logic
+    @timer = Timer.new
+    @target_fps = 60
   end
 
   def run
+    self.init
+    self.game_loop
+  ensure
+    self.close
   end
 
   def init
     @window.init &@game_logic.key_callback
     @game_logic.init
+    @timer.init
   end
 
   def close
+    @game_logic.clean_up
     @window.close
+  end
+
+  def game_loop
+    running = true
+    @timer.init
+    while running
+      interval = 1_000_000 / @target_fps
+      accum = @timer.time_elapsed
+      while accum < interval
+        sleep((interval - accum) / 1_000_000.0)
+        accum += @timer.time_elapsed
+      end
+
+      running = @game_logic.update(accum)
+      raise UnknownRunningStatusError if (running != true && running != false)
+    end
   end
 end
--- spec/engine/gameengine_spec.rb
+++ spec/engine/gameengine_spec.rb
@@ -9,6 +9,11 @@ describe GameEngine do
       end
       def init
       end
+      def clean_up
+      end
+      def update(interval)
+        return false
+      end
     end.new
 
     @game_engine = GameEngine.new(@title, @width, @height, @v_sync, @logic)
@@ -20,12 +25,16 @@ describe GameEngine do
   describe "attributes" do
     it { is_expected.to have_attributes(window: an_instance_of(Window)) }
     it { is_expected.to have_attributes(game_logic: @logic) }
+    it { is_expected.to have_attributes(timer: an_instance_of(Timer)) }
+    it { is_expected.to have_attributes(target_fps: 60) }
   end
 
   describe "methods" do
     it { is_expected.to respond_to(:init) }
     it { is_expected.to respond_to(:close) }
     it { is_expected.to respond_to(:run) }
+    it { is_expected.to respond_to(:game_loop) }
+    it { is_expected.to respond_to(:target_fps=) }
   end
 
   describe "window attributes" do
@@ -47,6 +56,7 @@ describe GameEngine do
         expect(block).to_not be_nil
       end
       expect(subject.game_logic).to receive(:init)
+      expect(subject.timer).to receive(:init)
       subject.init
     end
 
@@ -58,8 +68,106 @@ describe GameEngine do
   describe "#close" do
     before(:all) { @game_engine.close }
 
+    it "should close its attributes" do
+      expect(subject.game_logic).to receive(:clean_up)
+      expect(subject.window).to receive(:close)
+      subject.close
+    end
+
     it "should have destroyed the window" do
       expect(subject.window.handle).to be_nil
+    end
+  end
+
+  describe "#run" do
+    it "should call its own methods to open and close" do
+      is_expected.to receive(:init)
+      is_expected.to receive(:close)
+      is_expected.to receive(:game_loop)
+      subject.run
+    end
+  end
+
+  describe "#game_loop" do
+    it "should update game_logic with an integer time_interval" do
+      expect(subject.game_logic).to receive(:update).
+        with(an_instance_of Integer).and_call_original
+      subject.game_loop
+    end
+
+    it "should raise an error if game_logic.update doesn't return true or false" do
+      logic = Class.new do
+        def update interval
+        end
+      end.new
+      engine = GameEngine.new("Title",600,600,true,logic)
+
+      expect { engine.game_loop }.to raise_error UnknownRunningStatusError
+    end
+
+    it "should call update repeatedly until false is returned" do
+      num = rand(5)+5
+      logic = Class.new do
+        def initialize n
+          @count, @num = 0, n
+        end
+        def update interval
+          @count += 1
+          @count < @num
+        end
+      end.new num
+      engine = GameEngine.new("Title",600,600,true,logic)
+
+      expect(engine.game_logic).to receive(:update).exactly(num).times.and_call_original
+
+      engine.game_loop
+    end
+
+    it "should update at the target fps" do
+      logic = Class.new do
+        def initialize
+          @count = 0
+        end
+        def update interval
+          @count += 1
+          @count < 5
+        end
+      end.new
+      engine = GameEngine.new("Title",600,600,true,logic)
+
+      tar_int = 1_000_000 / engine.target_fps
+      err = 500
+
+      expect(engine.game_logic).to receive(:update).exactly(5).times.
+        with(be_between(tar_int - err, tar_int + err)).and_call_original
+
+      engine.game_loop
+    end
+
+    it "should pass the correct time interval" do
+      logic = Class.new do
+        attr_reader :diff
+        def initialize
+          @count = 0
+          @timer = Timer.new
+        end
+        def update interval
+          @count += 1
+          case @count
+          when 1
+            @timer.init
+            return true
+          when 2
+            @diff = interval - @timer.time_elapsed
+            return false
+          end
+        end
+      end.new
+      engine = GameEngine.new("Title",600,600,true,logic)
+
+      engine.game_loop
+
+      expect(logic.diff.abs).to be_between(0,500)
     end
   end
 end
