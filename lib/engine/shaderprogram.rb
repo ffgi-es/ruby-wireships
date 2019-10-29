72116e5aaf9fd7bdd87cae84db2ca829e37a3d32
Alastair Gilles <agilles91@gmail.com>
2019-10-29 13:22:46 +0100

    added testing and improved error raising for ShaderProgram

--- lib/engine/shaderprogram.rb
+++ lib/engine/shaderprogram.rb
@@ -4,7 +4,7 @@ OpenGL.load_lib
 
 include OpenGL
 
-class ShaderProgromError < StandardError
+class ShaderProgramError < StandardError
 end
 
 class ShaderProgram
@@ -15,19 +15,12 @@ class ShaderProgram
   end
 
   def build_program(sources)
-    sources.each { |type, source| self.create_shader type, source }
+    @shaders = []
+    sources.each { |type, source| @shaders << self.create_shader(type, source)}
     glLinkProgram(@program_id)
-
-    return_buf = ' ' * 4
-    glGetProgramiv(@program_id, GL_LINK_STATUS, return_buf)
-    return_value = return_buf.unpack('L').first
-    if return_value == 0
-      log_buf = ' ' * 2048
-      length_buf = ' ' * 4
-      glGetProgramInfoLog(@program_id, 2047, length_buf, log_buf)
-      message = "Error compiling shader\nLog:\n#{log_buf}"
-      raise ShaderProgramError, message
-    end
+    check_error(@program_id, GL_LINK_STATUS)
+    @shaders.each { |shader| glDetachShader(@program_id, shader) }
+    check_error(@program_id, GL_VALIDATE_STATUS)
   end
 
   def bind
@@ -38,6 +31,7 @@ class ShaderProgram
 
   def clean_up
     self.unbind
+    @shaders.each { |shader_id| glDeleteShader(shader_id) } if @shaders
     if @program_id != 0
       glDeleteProgram(@program_id)
       @program_id = 0
@@ -49,20 +43,28 @@ class ShaderProgram
     raise ShaderProgramError, "Unable to create shader: #{type}" if shader_id == 0
 
     glShaderSource(shader_id, 1, [source].pack('p'), [source.size].pack('I'))
-    
     glCompileShader(shader_id)
-    
+    check_error(shader_id, GL_COMPILE_STATUS)
+    glAttachShader(@program_id, shader_id)
+    return shader_id
+  end
+
+  def check_error id, type
+    refs = { GL_COMPILE_STATUS => ['Shader', 'Error compiling shader'],
+             GL_LINK_STATUS => ['Program', 'Error linking program'],
+             GL_VALIDATE_STATUS => ['Program', 'Error validating program'] }
+    call_type, error_type = refs[type]
+
     return_buf = ' ' * 4
-    glGetShaderiv(shader_id, GL_COMPILE_STATUS, return_buf)
+    send("glGet#{call_type}iv", id, type, return_buf)
+
     return_value = return_buf.unpack('L').first
-    if return_value == 0
-      log_buf = ' ' * 2048
+    if return_value == GL_FALSE
+      log_buf = ' ' * 10240
       length_buf = ' ' * 4
-      glGetShaderInfoLog(shader_id, 2047, length_buf, log_buf)
-      message = "Error compiling shader\nLog:\n#{log_buf}"
+      send("glGet#{call_type}InfoLog", id, 10239, length_buf, log_buf)
+      message = "#{error_type}\nLog:\n#{log_buf}"
       raise ShaderProgramError, message
     end
-
-    glAttachShader(@program_id, shader_id)
   end
 end
--- spec/engine/shaderprogram_spec.rb
+++ spec/engine/shaderprogram_spec.rb
@@ -27,4 +27,96 @@ describe ShaderProgram do
   describe "attributes" do
     it { is_expected.to have_attributes(program_id: a_value > 0) }
   end
+
+  describe "methods" do
+    it { is_expected.to respond_to(:build_program) }
+    it { is_expected.to respond_to(:bind) }
+    it { is_expected.to respond_to(:unbind) }
+    it { is_expected.to respond_to(:clean_up) }
+  end
+
+  describe "#build_program" do
+    it "should accept a hash of shaders" do
+      begin
+        prog = ShaderProgram.new
+        vertex_source = <<~SRC
+          #version 330
+          layout (location=0) in vec2 position;
+          layout (location=1) in vec3 colour;
+          out vec4 fColour;
+          uniform mat2 PVWMatrix;
+          void main() {
+            vec2 pos = PVWMatrix * position;
+            gl_Position = vec4(pos, 0.0, 1.0);
+            fColour = vec4(colour, 1.0);
+          }
+        SRC
+        fragment_source = <<~SRC
+          #version 330
+          in vec4 fColour;
+          out vec4 fragColour;
+          void main () {
+            fragColour = fColour;
+          }
+        SRC
+        shaders = { GL_VERTEX_SHADER => vertex_source,
+                    GL_FRAGMENT_SHADER => fragment_source }
+        expect{ prog.build_program(shaders) }.to_not raise_error
+      ensure
+        prog.clean_up
+      end
+    end
+
+    it "should raise an error if given incorrect shader code" do
+      begin
+        prog = ShaderProgram.new
+        vertex_source = <<~SRC
+          #version 330
+          layout (location=0) in vec2 position;
+          uniform mat2 PVWMatrix;
+          void main() {
+            vec2 pos = Matrix * position;
+            gl_Position = vec4(pos, 0.0, 1.0);
+          }
+        SRC
+        shaders = { GL_VERTEX_SHADER => vertex_source }
+        expect{ prog.build_program(shaders) }.to raise_error(
+          ShaderProgramError, /compiling/)
+      ensure
+        prog.clean_up
+      end
+    end
+
+    it "should raise an error if there is a problem linking" do
+      begin
+        prog = ShaderProgram.new
+        vertex_source = <<~SRC
+          #version 330
+          layout (location=0) in vec2 position;
+          layout (location=1) in vec3 colour;
+          uniform mat2 PVWMatrix;
+          out vec3 fgColour;
+          void main() {
+            vec2 pos = PVWMatrix * position;
+            gl_Position = vec4(pos, 0.0, 1.0);
+            fgColour = colour;
+          }
+        SRC
+        fragment_source = <<~SRC
+          #version 330
+          in ivec4 fColour;
+          out vec4 fragColour;
+          void main () {
+            fragColour = fColour;
+          }
+        SRC
+        shaders = { GL_VERTEX_SHADER => vertex_source,
+                    GL_FRAGMENT_SHADER => fragment_source }
+        expect{ prog.build_program(shaders) }.to raise_error(
+          ShaderProgramError, /linking/)
+      ensure
+        prog.clean_up
+      end
+    end
+  end
 end
